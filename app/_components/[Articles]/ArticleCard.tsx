15537e540aa1fa6f53c757866d67cd2025235d45
Vincent C <vinch.cm@gmail.com>
2024-11-21 18:21:21 +0100

    ArticleCard size updated to correspond to carousel

--- app/_components/[Articles]/ArticleCard.tsx
+++ app/_components/[Articles]/ArticleCard.tsx
@@ -10,8 +10,8 @@ export const ArticleCard = (props: {
   href: string;
 }) => {
   return (
-    <div className="articleCard w-64 h-128">
-      <img src={props.picture} alt={props.alt} className="flex items-center max-w-64 max-h-128 rounded-t-lg" />
+    <div className="articleCard w-80 h-160">
+      <img src={props.picture} alt={props.alt} className="flex items-center max-w-80 max-h-160 rounded-t-lg" />
       <div className="p-4">
         <h3 className="text-base font-semibold">{props.title}</h3>
         <p className="text-sm mt-2 line-clamp-3">{props.chapeau}</p>
