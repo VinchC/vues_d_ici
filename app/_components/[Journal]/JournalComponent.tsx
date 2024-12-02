0701fd46143fda2aefb1422988b26222a5220bc0
Vincent C <vinch.cm@gmail.com>
2024-12-02 15:49:41 +0100

    Dummary and pictureContainer done

--- app/_components/[Journal]/JournalComponent.tsx
+++ app/_components/[Journal]/JournalComponent.tsx
@@ -11,21 +11,28 @@ export const JournalComponent = (props: JournalProps) => {
             #{props.id} - N° de {props.title}
           </h2>
           <p className="paragraph">{props.chapeau}</p>
-        </Card>
-        <ul>
-          {props.content.map((e, index) => (
-            <>
-              <li key={index}>{e.categoryName}</li>
-              <ul className="listStyle">
-                {e.text.map((f, index) => (
-                  <li className="chartText" key={index}>
-                    {f}
-                  </li>
+          <div className="flex max-sm:flex-col-reverse max-sm:flex-col items-center max-sm:items-start mb-8">
+            <div className="flex-[3] flex-col gap-2">
+              <ul>
+                {props.content.map((e, index) => (
+                  <>
+                    <li className="font-bold" key={index}>{e.categoryName}</li>
+                    <ul className="listStyle">
+                      {e.text.map((f, index) => (
+                        <li className="text" key={index}>
+                          {f}
+                        </li>
+                      ))}
+                    </ul>
+                  </>
                 ))}
               </ul>
-            </>
-          ))}
-        </ul>
+            </div>
+            <div className="flex-[2] justify-around items-center max-md:m-auto ml-auto">
+              <div className="max-lg:w-52 max-lg:h-52 w-80 h-80 bg-red-500 mt-4"></div>
+            </div>
+          </div>
+        </Card>
       </section>
     </>
   );
